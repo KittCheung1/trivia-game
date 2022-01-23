87690c3859c07ba507d25d84d4f22c787f69d769
Kitt Cheung <kitt.cheung@live.se>
2022-01-23 19:48:32 +0100

    feat: add App vue

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,34 @@
 <script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
+
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <div id="nav">
+    <router-link :to="{name:'Start'}">Start</router-link>
+    <router-link :to="{name:'Trivia'}">Trivia</router-link>
+    <router-link :to="{name: 'Record'}">Record</router-link>
+  </div>
+  <router-view></router-view>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+
+#app{
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+#nav{
+  padding: 30px;
+}
+
+#nav a{
+  font-weight: bold;
+  color: rgb(37, 39, 43);
+  padding: 10px;
+  border-radius: 4px;
+}
+
+#nav a.router-link-exact-active{
+  color: white;
+  background: rgb(29, 29, 196);
 }
 </style>
