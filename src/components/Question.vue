2f151d3a9136b8cfcc275b4a5578dbec006f907f
Kitt Cheung <kitt.cheung@live.se>
2022-02-07 22:52:12 +0100

    FEAT: Loading in questions

--- src/components/Question.vue
+++ src/components/Question.vue
@@ -1,29 +1,55 @@
 <script setup>
+import { currentUser } from "../api/users";
+import { CATEGORIES_URL } from "../api/index";
+import { onMounted, reactive } from "vue";
+import { computed } from "vue";
 import { useStore } from 'vuex';
 
+
 const store = useStore()
+
+const questionList = store.state.listOfQuestions
 const questions = store.getters.questions;
-let result = questions.map(a => a.question);
-console.log(result);
 
+onMounted(() => {
+    store.dispatch("loadQuestions")
 
-// let test = await store.dispatch("loadQuestions");
-// console.log(test);
-// let result = test.map(a => a.question);
-// console.log(result);
+});
+
 
 </script>
 
 <template>
-<div> 
-    
-<p>{{questions}}</p>
-<p>hejeje</p>
-
-</div>
-
+    <div>
+        <p>{{ questions }}</p>
+        <p>hejeje</p>
+        <div>
+            <div class="questionDiv">
+                <ul>
+                    <li v-for="quest in questionList">{{ quest }}</li>
+                </ul>
+                <button type="button" class="btn" @click="">click me</button>
+            </div>
+            <p>{{ store.state.getQuestions }}help</p>
+        </div>
+    </div>
 </template>
 
-<style>
+<style scoped>
+.questionDiv {
+    border-width: 3px;
+    border-style: solid;
+    border-color: black;
+    text-align: left;
+}
 
+.btn {
+    width: 100px;
+    height: 80px;
+    border-style: solid;
+    border-color: black;
+    border-radius: 3px;
+    border-width: 2px;
+    background-color: rgb(102, 102, 223);
+}
 </style>
