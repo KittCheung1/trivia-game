b4f10c54133d9d07d09f33dbce6fbc8fde54ca29
Kitt Cheung <kitt.cheung@live.se>
2022-02-22 14:47:56 +0100

    FEAT: half finished btn loading feature

--- src/components/Question.vue
+++ src/components/Question.vue
@@ -32,23 +32,41 @@ const onClick = () => {
     store.commit('setIndex')
 }
 
+const exitBtn = () => {
+    store.commit('setUser', null);
+    store.commit('setIndex', 0);
+    //    store.commit('setCurrentScore', 0);
+    //    store.commit('setHighScore', 0);
+    router.push('/');
+}
+
+const createAnswerArray = (correct, incorrect) => {
+    let answerArray = correct.concat(incorrect);
+    return answerArray;
+}
+
+
 </script>
 
 <template>
     <div v-if="isLoaded">
         <!-- <p>{{ questionObjects[questionIndex].question }}</p>
-        <p>{{ questionObjects[questionIndex].correct_answer }}</p> -->
+        <p>{{ questionObjects[questionIndex].correct_answer }}</p>-->
         <p>hejeje</p>
         <div>
             <div class="questionDiv">
-                <ul>
-                    <p>{{ questionObjects[questionIndex].question }}</p>
-                    <p>{{ questionObjects[questionIndex].correct_answer }}</p>
-                    <p>{{ questionObjects[questionIndex].incorrect_answer }}</p>
-                    <button type="button" class="btn" @click="onClick">Click Me</button>
+                <p>{{ questionObjects[questionIndex].question }}</p>
+                <div>
+                    <button
+                        type="button"
+                        class="btn"
+                        v-for="ans in createAnswerArray(questionObjects[0].correct_answer, questionObjects[0].incorrect_answers)"
+                        @click="onClick"
+                    >Click Me</button>
                     <!-- <QuestionDisplay v-if="questionObjects.length!=0" :question="question.value" @answered="incrementQuestionCounter"/> -->
-                </ul>
+                </div>
             </div>
+            <button type="button" class="exitBtn" @click="exitBtn">Exit Game</button>
         </div>
     </div>
     <div v-else>LOADING</div>
